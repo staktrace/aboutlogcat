d93fd2dd597eae5da71468bc14c5415ba1afcd51
diegocr <dcasorran@gmail.com>
2013-12-09 19:44:16 +0000

    styling and some fixes/improvements

--- bootstrap.js
+++ bootstrap.js
@@ -7,9 +7,9 @@ Cm.QueryInterface(Ci.nsIComponentRegistrar);
 var gWindow = null;
 var gMenuId = -1;
 var gDexLoaded = false;
+var gJavaCodeURI = null;
 
 Cu.import("resource://gre/modules/Services.jsm");
-Cu.import("resource://gre/modules/AddonManager.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource://gre/modules/JNI.jsm");
 
@@ -29,49 +29,65 @@ AboutLogcat.prototype = {
     classDescription: "about:logcat",
     classID: Components.ID("{2ccd132e-7fc6-4a52-a766-9e5b61c1cb03}"),
     contractID: "@mozilla.org/network/protocol/about;1?what=logcat",
- 
+
     newChannel: function(uri) {
-		var reverse = gPrefService.getPrefType('reverse') && gPrefService.getBoolPref('reverse');
-		var html = gPrefService.getPrefType('html') && gPrefService.getBoolPref('html');
-		var filter = gPrefService.getPrefType('filter') && gPrefService.getCharPref('filter');
-        var logcat = 'NO LOGCAT AVAILABLE';
+        let reverse = gPrefService.getPrefType('reverse') && gPrefService.getBoolPref('reverse');
+        let html = gPrefService.getPrefType('html') && gPrefService.getBoolPref('html');
+        let filter = gPrefService.getPrefType('filter') && gPrefService.getCharPref('filter');
+        let logcat = 'NO LOGCAT AVAILABLE', fcb;
         try {
             logcat = gWindow.sendMessageToJava({ type: "logcat:get" });
         } catch (e) {
             logcat = 'Error obtaining logcat: ' + e;
         }
-		
-		var fcb = filter ? (filter[0] == '/' ? (() => {
-			try {
-				filter = new RegExp(filter);
-			} catch(e) {
-				filter = /./;
-			}
-			return s => filter.test(s);
-		}) : s => ~s.indexOf(filter)) : s => s;
-		
-		if(html) {
-			logcat = logcat.split("\n");
-			if(reverse) logcat = logcat.reverse();
-			logcat = logcat.map(ln => fcb(ln) && '<div class="' + ln.split(" ")[4] + '">' + ln + '</div>' || '').filter(String);
-			var n = logcat.length;
-			logcat = '<html><head><style type="text/css">'
-				+ '.V{background-color:#eee}'
-				+ '.D{background-color:#abc}'
-				+ '.I{background-color:#def}'
-				+ '.W{background-color:#ffd}'
-				+ '.E{background-color:#fdd}'
-				+ '.F{background-color:#f00}'
-				+ 'div{border-bottom:1px solid #444}'
-				+ '</style></head><body><h3>Showing ' + n + ' entries.</h3>' + logcat.join("");
-			var content = 'data:text/html,';
-		} else {
-			var content = 'data:text/plain,';
-		}
 
-        var ioService = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
-        var channel = ioService.newChannel(content + encodeURIComponent(logcat), null, null);
-        // var securityManager = Cc["@mozilla.org/scriptsecuritymanager;1"].getService(Ci.nsIScriptSecurityManager);
+        if (filter) {
+
+            if (filter[0] == '/') {
+                fcb = (() => {
+                    try {
+                        filter = new RegExp(filter);
+                    } catch (e) {
+                        filter = /./;
+                    }
+                    return s => filter.test(s);
+                });
+            } else {
+                fcb = s => ~s.indexOf(filter);
+            }
+
+        } else {
+
+            fcb = s => s;
+        }
+
+        if (html) {
+            let c = Cc["@mozilla.org/intl/scriptableunicodeconverter"]
+                .createInstance(Ci.nsIScriptableUnicodeConverter);
+            c.charset = 'UTF-8';
+            let p = Cc["@mozilla.org/parserutils;1"].getService(Ci.nsIParserUtils);
+            logcat = p.convertToPlainText(c.ConvertFromUnicode(logcat),0xff,0).split("\n");
+            if (reverse) {
+                logcat = logcat.reverse();
+            }
+            logcat = logcat.map(ln => fcb(ln) && '<div class="' + ln.split(" ")[4] + '">' + ln + '</div>' || '').filter(String);
+            var n = logcat.length;
+            logcat = '<html><head><style type="text/css">'
+                + '.V{background-color:#eee}'
+                + '.D{background-color:#abc}'
+                + '.I{background-color:#def}'
+                + '.W{background-color:#ffd}'
+                + '.E{background-color:#fdd}'
+                + '.F{background-color:#f00}'
+                + 'div{border-bottom:1px solid #444}'
+                + 'html,body{margin:0 auto}'
+                + '</style></head><body><h3>Showing ' + n + ' entries.</h3>' + logcat.join("");
+            var content = 'data:text/html;charset=UTF-8,';
+        } else {
+            var content = 'data:text/plain,';
+        }
+
+        var channel = Services.io.newChannel(content + encodeURIComponent(logcat), null, null);
         channel.originalURI = uri;
         return channel;
     },
@@ -95,9 +111,7 @@ function attachTo(aWindow) {
         });
     }
     if (!gDexLoaded) {
-        AddonManager.getAddonByID("projects.aboutlogcat.ffext@staktrace.com", function(addon) {
-            gWindow.NativeWindow.loadDex(addon.getResourceURI('java-code.jar').spec, 'LogcatGrabber');
-        });
+        gWindow.NativeWindow.loadDex(gJavaCodeURI, 'LogcatGrabber');
         gDexLoaded = true;
     }
 }
@@ -109,9 +123,7 @@ function detachFrom(aWindow) {
         Cm.unregisterFactory(AboutLogcat.prototype.classID,
                 AboutLogcatFactory);
         if (gDexLoaded) {
-            AddonManager.getAddonByID("projects.aboutlogcat.ffext@staktrace.com", function(addon) {
-                gWindow.NativeWindow.unloadDex(addon.getResourceURI('java-code.jar').spec);
-            });
+            gWindow.NativeWindow.unloadDex(gJavaCodeURI);
             gDexLoaded = false;
         }
         gWindow = null;
@@ -120,11 +132,7 @@ function detachFrom(aWindow) {
 
 var browserListener = {
     onOpenWindow: function(aWindow) {
-        var win = aWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindow);
-        win.addEventListener("UIReady", function listener(aEvent) {
-            win.removeEventListener("UIReady", listener, false);
-            attachTo(win);
-        }, false);
+        attachToStub(aWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindow));
     },
 
     onCloseWindow: function(aWindow) {
@@ -135,16 +143,28 @@ var browserListener = {
     }
 };
 
-function startup(aData, aReason) {
-    var enumerator = Services.wm.getEnumerator("navigator:browser");
-    while (enumerator.hasMoreElements()) {
-        // potential race condition here - the window may not be ready yet at
-        // this point, so ideally we would test for that. but i can't find a
-        // property that reflects whether or not UIReady has been fired, so
-        // for now just assume the window is ready
-        attachTo(enumerator.getNext().QueryInterface(Ci.nsIDOMWindow));
+function attachToStub(aWindow) {
+
+    if (aWindow.document.readyState == "complete") {
+        attachTo(aWindow);
+    } else {
+        aWindow.addEventListener("UIReady", function listener() {
+            aWindow.removeEventListener("UIReady", listener, false);
+            attachTo(aWindow);
+        }, false);
     }
-    Services.wm.addListener(browserListener);
+}
+
+function startup(aData, aReason) {
+    let {AddonManager} = Cu.import("resource://gre/modules/AddonManager.jsm", {});
+    AddonManager.getAddonByID(aData.id, function(aAddon) {
+        gJavaCodeURI = aAddon.getResourceURI('java-code.jar').spec;
+        let enumerator = Services.wm.getEnumerator("navigator:browser");
+        while (enumerator.hasMoreElements()) {
+            attachToStub(enumerator.getNext().QueryInterface(Ci.nsIDOMWindow));
+        }
+        Services.wm.addListener(browserListener);
+    });
 }
 
 function shutdown(aData, aReason) {
