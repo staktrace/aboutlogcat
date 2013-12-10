c8d1face6247b0e6d89646a088c4b0968b6a4901
Kartikaya Gupta <kgupta@mozilla.com>
2013-12-10 07:19:56 -0500

    Update packaging script to include the new files

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@
 mkdir classes
 javac -source 1.6 -target 1.6 -d classes -cp $HOME/android/sdk/platforms/android-17/android.jar LogcatGrabber.java
 $HOME/android/sdk/platform-tools/dx --dex --verbose --output=java-code.jar classes LogcatGrabber.java
-zip aboutlogcat.xpi java-code.jar install.rdf bootstrap.js
+zip aboutlogcat.xpi java-code.jar install.rdf bootstrap.js icon.png options.xul
 rm -rf classes java-code.jar
 
 adb push aboutlogcat.xpi /mnt/sdcard/
